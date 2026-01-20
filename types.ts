031ad03d7274297a67ab857ea0180774da75dd8e
Daniel Carlman <daniel.v.carlman@gmail.com>
2026-01-20 10:37:40 -0600

    Define register schemas

--- types.ts
+++ types.ts
@@ -1,4 +1,24 @@
 import { z } from "zod";
+import { usersTable } from "./schema";
+import { createSelectSchema, createInsertSchema } from "drizzle-zod";
 export const envSchema = z.object({
   DB_URL: z.string(),
 });
+
+export const userSchema = createSelectSchema(usersTable);
+export type User = z.infer<typeof userSchema>;
+
+export const userInsertSchema = createInsertSchema(usersTable);
+export type UserInsert = z.infer<typeof userInsertSchema>;
+
+export const registerInputSchema = userInsertSchema.pick({
+  username: true,
+  password: true,
+});
+export type RegisterInput = z.infer<typeof registerInputSchema>;
+
+export const registerOutputSchema = userSchema.pick({
+  id: true,
+  username: true,
+});
+export type RegisterOutput = z.infer<typeof registerOutputSchema>;
