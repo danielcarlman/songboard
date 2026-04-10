6fbb601eb5702439d459c3413c36585451ead3d2
Daniel Carlman <daniel.v.carlman@gmail.com>
2026-04-10 13:32:26 -0500

    Test delete song on button click

--- test/DeleteButton.test.tsx
+++ test/DeleteButton.test.tsx
@@ -1,20 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import DeleteButton from "../components/DeleteButton";
 import { useState } from "react";
 import { Song } from "@/types";
 
+const mockSongs: Song[] = [
+  { id: "1", userId: "abc", lyrics: "lala", title: "hello" },
+  { id: "2", userId: "abc", lyrics: "lala", title: "world" },
+];
+
 function DeleteButtonWrapper() {
-  const [_, setSongs] = useState<Song[]>([
-    { id: "1", userId: "abc", lyrics: "lala", title: "hello" },
-  ]);
-  return <DeleteButton songId="" setSongs={setSongs} />;
+  const [_, setSongs] = useState<Song[]>([mockSongs[0]]);
+  return <DeleteButton songId="1" setSongs={setSongs} />;
+}
+
+function DeleteButtonWrapperWithTwoSongs() {
+  const [songs, setSongs] = useState<Song[]>([...mockSongs]);
+  return (
+    <>
+      {songs.map((song) => (
+        <DeleteButton key={song.id} songId={song.id} setSongs={setSongs} />
+      ))}
+    </>
+  );
 }
 
 describe("<DeleteButton />", () => {
   test("should add <button/> to the page", () => {
     render(<DeleteButtonWrapper />);
-
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
   });
@@ -30,5 +43,16 @@ describe("<DeleteButton />", () => {
     render(<DeleteButtonWrapper />);
     const button = screen.getByRole("button");
     expect(button).toHaveTextContent("Delete Song");
+  });
+
+  test("should delete the song on button click", async () => {
+    global.fetch = jest.fn(() => Promise.resolve({ ok: true })) as jest.Mock;
+
+    render(<DeleteButtonWrapperWithTwoSongs />);
+    const buttons = screen.getAllByRole("button");
+    await act(async () => {
+      await userEvent.click(buttons[0]);
+    });
+    expect(screen.getAllByRole("button")).toHaveLength(1);
   });
 });
